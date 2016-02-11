5d8d875d2a0103b57be19f96a88ad22b19cd808c
Frankie DiPietro <fad4470@rit.edu>
2016-02-11 13:42:23 -0500

    Add asteroid spawning

---
+++ Assets/Scripts/Enemy/Asteroid.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Called by custom AnimationEvent
+	void OnAnimationEnd() {
+		Destroy (this.gameObject);
+	}
+}
