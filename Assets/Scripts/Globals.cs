7c88fff370202f221f93d31b9c9508d800b39c9e
Frankie DiPietro <fad4470@rit.edu>
2016-02-11 13:04:05 -0500

    Add assets and moon moving

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Globals : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject canvas;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGameOver() {
+		canvas.SendMessage ("OnGameOver");
+	}
+}
